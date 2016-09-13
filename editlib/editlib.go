c4bf18b04deb31a6add9f02624e26777db3716a8
andresvia <andres.via@gmail.com>
2016-09-13 12:30:14 -0700

    writing some actual tests

---
+++ editlib/editlib.go
@@ -0,0 +1,6 @@
+package editlib
+
+func Edit(initialValue, startFlag, endFlag string, insert ...string) (final_value string, changed bool) {
+	final_value = initialValue
+	return
+}
--- editlib/editlib_test.go
+++ editlib/editlib_test.go
@@ -9,16 +9,42 @@ func TestEdit(t *testing.T) {
 
 	Convey("Given start, termination and some text to insert", t, func() {
 
-		Convey("With an initial text", func() {
+		start_text := "# EDITLIB START"
+		termination_text := "# EDITLIB TERMINATE"
+		to_insert := "hello world"
 
-			Convey("Should append new text", nil)
+		Convey("The final result should be the same", func() {
+			final_text := `I want to be edited.
+# EDITLIB START
+hello world
+# EDITLIB TERMINATE`
 
-		})
+			Convey("With an initial text", func() {
 
-		Convey("With an existing text and some text to insert", func() {
+				initial_text := "I want to be edited."
 
-			Convey("Should edit existing text in place", nil)
+				Convey("The text is appended", func() {
+					edited_text, edited := Edit(initial_text, start_text, termination_text, to_insert)
+					So(edited_text, ShouldEqual, final_text)
+					So(edited, ShouldBeFalse)
+				})
 
+			})
+
+			Convey("Or with an existing text", func() {
+
+				existing_text := `I want to be edited.
+# EDITLIB START
+replace me
+# EDITLIB TERMINATE`
+
+				Convey("The text is edited in place", func() {
+					edited_text, edited := Edit(existing_text, start_text, termination_text, to_insert)
+					So(edited_text, ShouldEqual, final_text)
+					So(edited, ShouldBeFalse)
+				})
+
+			})
 		})
 
 		Convey("With an already edited text", func() {
